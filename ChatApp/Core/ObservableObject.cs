943edfb57022afa6cc56a5dd671be6d68af4c274
Mohamed Omran <mohamed.ommran@gmail.com>
2022-06-14 16:10:24 +0200

    Project Commit

---
+++ ChatApp/Core/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ChatApp.Core
+{
+
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,109 @@
+﻿using ChatApp.Core;
+using ChatApp.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts  { get; set; }
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+        private ContactModel _selectedContact;
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set 
+            {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set 
+            { _message = value;
+              OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+            //instantiate the relay command
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+                // to empty the textbox
+                Message = "";
+            });
+            Messages.Add(new MessageModel
+            {
+                Username = "Alice",
+                UsernameColor = "#409aff",
+                ImageSource = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+                Message = "Test",
+                Date = DateTime.Now,
+                IsNativeMessage = false,
+                FirstMessage = true
+            });
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Alice",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+                    Message= "Test",
+                    Date = DateTime.Now,
+                    IsNativeMessage = false,
+                    FirstMessage = false
+                });
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Hani",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+                    Message = "Test",
+                    Date = DateTime.Now,
+                    IsNativeMessage = false,
+                    FirstMessage = false
+                });
+            }
+            Messages.Add(new MessageModel
+            {
+                Username = "Hani",
+                UsernameColor = "#409aff",
+                ImageSource = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+                Message = "Test",
+                Date = DateTime.Now,
+                IsNativeMessage = false,
+                FirstMessage = false
+            });
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username= $"Alice{i}",
+                    ImageSource= "https://image.shutterstock.com/image-photo/stock-photo-head-shot-young-attractive-businessman-in-glasses-standing-in-modern-office-pose-for-camera-250nw-1854697390.jpg",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
